712999e5854db4ebaeb9c600df67f199804de221
Oiciruamz <mauricio96aguirre@gmail.com>
2024-05-12 21:47:07 -0600

    Video 28

--- SistemaVenta.AplicacionWeb/Controllers/AccesoController.cs
+++ SistemaVenta.AplicacionWeb/Controllers/AccesoController.cs
@@ -11,7 +11,7 @@ using Microsoft.AspNetCore.Mvc.Routing;
 
 namespace SistemaVenta.AplicacionWeb.Controllers
 {
-    
+
     public class AccesoController : Controller
     {
         private readonly IUsuarioService _usuarioServicio;
@@ -26,18 +26,26 @@ namespace SistemaVenta.AplicacionWeb.Controllers
             ClaimsPrincipal claimUser = HttpContext.User;
             if (claimUser.Identity.IsAuthenticated)
             {
-               
+
                 return RedirectToAction("Index", "Home");
             }
 
             return View();
         }
 
+        public IActionResult RestablecerClave()
+        {
+
+            return View();
+        }
+
+
+
         [HttpPost]
-        public async Task<IActionResult>Login(VMUsuarioLogin modelo)
+        public async Task<IActionResult> Login(VMUsuarioLogin modelo)
         {
             Usuario usuario_encontrado = await _usuarioServicio.ObtenerPorCrendenciales(modelo.Correo, modelo.Clave);
-            if(usuario_encontrado == null)
+            if (usuario_encontrado == null)
             {
                 ViewData["Mensaje"] = "No se encontraron coincidencias";
                 return View();
@@ -47,21 +55,65 @@ namespace SistemaVenta.AplicacionWeb.Controllers
             new Claim(ClaimTypes.Name, usuario_encontrado.Nombre),
              new Claim(ClaimTypes.NameIdentifier, usuario_encontrado.IdUsuario.ToString()),
             new Claim(ClaimTypes.Role, usuario_encontrado.IdRol.ToString()),
-            new Claim("UrlFoto", usuario_encontrado.UrlFoto),}; 
-            
+            new Claim("UrlFoto", usuario_encontrado.UrlFoto),};
+
             ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
 
-            AuthenticationProperties properties = new AuthenticationProperties() { 
+            AuthenticationProperties properties = new AuthenticationProperties() {
                 AllowRefresh = true,
                 IsPersistent = modelo.MantenerSesion,
-            
+
             };
             await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity),
                 properties
                 );
 
             return RedirectToAction("Index", "Home");
-        
+
         }
+
+
+        [HttpPost]
+
+
+        public async Task<IActionResult> RestablecerClave(VMUsuarioLogin modelo)
+        {
+            try
+            {
+                string urlPlantillaCorreo = $"{this.Request.Scheme}://{this.Request.Host}/Plantilla/RestablecerClave?clave=[clave]";
+
+                bool resultado = await _usuarioServicio.RestablecerClave(modelo.Correo, urlPlantillaCorreo);
+
+                if (resultado)
+                {
+                    ViewData["Mensaje"] = "Listo, su contraseña fue establecida. Revise su correo";
+                    ViewData["MensajeError"] = null;
+                }
+                else
+                {
+                    ViewData["MensajeError"] = "Tenemos problemas. Por favor intentelo de nuevo mas tarde";
+                    ViewData["Mensaje"] = null;
+                }
+
+            }
+            catch(Exception ex)
+            {
+                ViewData["MensajeError"] = ex.Message;
+                ViewData["Mensaje"] = null;
+            }
+            return View();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
